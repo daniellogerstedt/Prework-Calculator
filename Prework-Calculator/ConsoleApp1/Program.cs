fce1be7f446444d29f70266b06c0cfc1e59abfe9
daniellogerstedt <daniel.logerstedt@gmail.com>
2019-03-18 12:12:00 -0700

    Refactored how the calculator handles input.

--- Prework-Calculator/ConsoleApp1/Program.cs
+++ Prework-Calculator/ConsoleApp1/Program.cs
@@ -10,23 +10,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            if (args.Length == 3 && int.TryParse(args[0], out int a) && int.TryParse(args[1], out int b))
-                switch (args[2])
+            Console.WriteLine("Please Enter The First Number:");
+            string first = Console.ReadLine();
+
+            Console.WriteLine("Please Enter The Second Number:");
+            string second = Console.ReadLine();
+
+            Console.WriteLine("Please Enter The Action:");
+            string action = Console.ReadLine();
+
+            if (int.TryParse(first, out int a) && int.TryParse(second, out int b))
+                switch (action)
                 {
                     case "add":
-                        Console.WriteLine(Program.add(a, b));
+                        Console.WriteLine("Your answer is: " + Program.add(a, b));
                         break;
 
                     case "subtract":
-                        Console.WriteLine(Program.subtract(a, b));
+                        Console.WriteLine("Your answer is: " + Program.subtract(a, b));
                         break;
 
                     case "multiply":
-                        Console.WriteLine(Program.multiply(a, b));
+                        Console.WriteLine("Your answer is: " + Program.multiply(a, b));
                         break;
 
                     case "divide":
-                        Console.WriteLine(Program.divide(a, b));
+                        Console.WriteLine("Your answer is: " + Program.divide(a, b));
                         break;
 
                     default:
@@ -34,6 +43,9 @@ namespace ConsoleApp1
                         break;
                 }
             else Console.WriteLine("Please provide 2 integers and an action (add, subtract, multiply, divide).");
+
+            Console.WriteLine("Press Return/Enter to exit.");
+            Console.ReadLine();
         }
 
         static int add(int a, int b)
