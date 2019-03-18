4fa4e5fe9dbd1059ab83f7b54c7a07c811b84c0d
unknown <daniel.logerstedt@gmail.com>
2019-03-18 09:41:37 -0700

    Main completed with switch case

---
+++ Prework-Calculator/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+                switch (args[2])
+                {
+                    case "add":
+                        Console.WriteLine(Program.add(args[0], args[1]));
+                        break;
+
+                    case "subtract":
+                        Console.WriteLine(Program.subtract(args[0], args[1]));
+                        break;
+
+                    case "multiply":
+                        Console.WriteLine(Program.multiply(args[0], args[1]));
+                        break;
+
+                    case "divide":
+                        Console.WriteLine(Program.divide(args[0], args[1]));
+                        break;
+
+                    default:
+                        Console.WriteLine("Please provide 2 numbers and an action (add, subtract, multiply, divide).");
+                        break;
+                }
+            else Console.WriteLine("Please provide 2 numbers and an action (add, subtract, multiply, divide).");
+        }
+
+    }
+}
